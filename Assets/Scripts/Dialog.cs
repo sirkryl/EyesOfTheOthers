a9c7dac98300575c8eba169e1e4dbf15380d88b4
sirkryl <chrisvhuber@gmail.com>
2014-10-14 01:18:16 +0200

    dialog xml and other stuff, very very experimental

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -1,27 +1,58 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
-
-public class Dialog : MonoBehaviour {
+using System.Collections.Generic;
+public class Dialog : DialogIO {
 	private GameObject player;
 	public KeyCode pressButton = KeyCode.E;
 	private Camera mainCamera;
+	public string name;
+	public DialogData dialogData;
+	private bool inDialog = false;
 	private bool inDialogRange = false;
 	public float dialogDistance = 2.0f;
 	static float closestDialogDistance = 999.0f;
 	static GameObject bestOption;
+	public Text npcName;
+	public Text npcText;
+	public Text playerText;
+	public Text playerName;
+	public Text dialogOptions;
+	public GameObject dialogElementPrefab;
+	public GameObject dialogList;
+	private int currentId;
+	private bool startedDialog = false;
 	//public Canvas dialogCanvas;
 	private float distance;
 	// Use this for initialization
 	void Start () {
 		mainCamera = Camera.main;
-		player = GameObject.FindWithTag ("Player");
+		player = GameObject.FindWithTag ("Player") as GameObject;
+		dialogList = GameObject.Find ("DialogList") as GameObject;
+		//dialogElementPrefab = Resources.Load ("Prefabs/DialogElement") as GameObject;
+		/*npcName = GameObject.Find ("NPCName") as Text;
+		npcText = GameObject.Find ("NPCText") as Text;
+		playerText = GameObject.Find ("PlayerText") as Text;
+		playerName = GameObject.Find ("PlayerName") as Text;
+		dialogOptions = GameObject.Find ("DialogOptions") as Text;*/
 
 	}
 
 	void OnGUI() {
-		
-		if (inDialogRange)
+
+		if (inDialog && startedDialog)
+		{
+			Image dialogImage = Instantiate (Resources.Load ("Prefabs/DialogElement", typeof(Image))) as Image;
+			dialogImage.transform.SetParent (dialogList.transform, false);
+			Text[] texts = dialogImage.GetComponentsInChildren<Text>();
+			texts[0].text = dialogData.characterName + ":";
+			texts[1].text = dialogData.dialogText[0].text;
+			startedDialog = false;
+			//playerName.text = "You:";
+
+		}
+
+		if (inDialogRange && !inDialog)
 		{
 			GUI.Box (new Rect ((float)(Screen.width*0.5-100),(float)(Screen.height*0.5-20),200,20), "Press E to talk.");
 		}
@@ -50,7 +81,15 @@ public class Dialog : MonoBehaviour {
 			{
 				if (bestOption == gameObject)
 				{
+					if (dialogData == null)
+					{
+						dialogData = Load (name);
+
+					}
+					inDialog = true;
+					startedDialog = true;
 					player.GetComponent<DisplayWindows>().ShowDialogWindow();
+
 					//player.GetComponent<MouseLook>().enabled = false;;
 					//mainCamera.GetComponent<MouseLook>().enabled = false;
 					//dialogCanvas.GetComponent<Canvas>().enabled = true;
---
+++ Assets/Scripts/DialogIO.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text;
+using System;
+using System.Collections.Generic;
+public class DialogIO : MonoBehaviour {
+
+	//public static Dictionary<string, DialogData> dialogMap;
+
+	[XmlRoot("dialog")]
+	public class DialogData {
+		[XmlAttribute]
+		public string characterName;
+		[XmlElement("text")]
+		public DialogText[] dialogText;
+		[XmlElement("choice")]
+		public DialogChoice[] dialogChoice;
+	}
+
+	public class DialogText {
+		[XmlAttribute]
+		public int id;
+		[XmlAttribute]
+		public int leadsTo;
+		[XmlText]
+		public string text;
+	}
+
+	public class DialogChoice {
+		[XmlAttribute]
+		public int id;
+		[XmlText]
+		public string text;
+		[XmlElement("answer")]
+		public DialogAnswer[] dialogAnswers;
+	}
+
+	public class DialogAnswer {
+		[XmlAttribute]
+		public int id;
+		[XmlAttribute]
+		public int leadsTo;
+		[XmlText]
+		public string text;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*Debug.Log ("Name:" +data.characterName);
+		Debug.Log ("Text size: "+data.dialogText.Length);
+		for(int i = 0; i < data.dialogText.Length; i++)
+		{
+			Debug.Log ("Text ids: "+data.dialogText[i].id);
+			Debug.Log ("Text content: "+data.dialogText[i].text);
+		}
+		Debug.Log ("Choice size: "+data.dialogChoice.Length);
+		for(int i = 0; i < data.dialogChoice.Length; i++)
+		{
+			Debug.Log ("Choice ids: "+data.dialogChoice[i].id);
+			Debug.Log ("Choice size: "+data.dialogChoice[i].dialogAnswers.Length);
+			for (int j = 0; j < data.dialogChoice[i].dialogAnswers.Length; j++)
+			{
+				Debug.Log ("Answer ids: "+data.dialogChoice[i].dialogAnswers[j].id);
+			}
+		}*/
+	}
+
+	public DialogData Load(string charName)
+	{
+		XmlSerializer xmlSerializer = new XmlSerializer(typeof(DialogData));
+		FileStream ReadFileStream = new FileStream("Assets/Resources/Dialog/mechanic.xml", FileMode.Open, FileAccess.Read, FileShare.Read);
+		DialogData data = (DialogData)xmlSerializer.Deserialize(ReadFileStream);
+		ReadFileStream.Close();
+		return data;
+		//Debug.Log ("Count: "+dialogMap.Count);
+		//dialogMap.Add (charName, data);
+	}
+}
